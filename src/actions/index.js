348cb1ecfdab701b4d83fed5e02950264c808753
erozonachi <erozonachi.james@gmail.com>
2019-06-19 15:02:39 +0100

    Import and use axios in action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-// we'll need axios
+import axios from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
@@ -10,5 +10,7 @@ export const FAILURE = 'FAILURE';
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
 export const getCharacters = () => dispatch => {
-  
+  axios.get('https://swapi.co/api/people/')
+  .then(res => null)
+  .catch(error => null);
 }
