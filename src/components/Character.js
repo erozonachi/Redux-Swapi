c7e121974489d2825b59685ef21782a0d8d30dab
erozonachi <erozonachi.james@gmail.com>
2019-06-19 16:25:12 +0100

    Import and use prop-types in character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Character = props => {
   return <li>{props.character.name}</li>;
 };
+
+Character.propTypes = {
+  character: PropTypes.objectOf(PropTypes.shape({name: PropTypes.string})).isRequired
+}
 
 export default Character;
