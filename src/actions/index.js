d67eaa3cbf5ef4bc3fb87166fa3122aa05d4e964
erozonachi <erozonachi.james@gmail.com>
2019-06-19 15:09:09 +0100

    Complete the get-characters action creator's logic

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,8 @@ export const FAILURE = 'FAILURE';
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
 export const getCharacters = () => dispatch => {
+  dispatch({type: FETCHING});
   axios.get('https://swapi.co/api/people/')
-  .then(res => null)
-  .catch(error => null);
+  .then(res => dispatch({type: SUCCESS, payload: res.data}))
+  .catch(error => dispatch({type: FAILURE, payload: {error: error.message}}));
 }
